1edf0e056fd23a3ded86726a8064e8d86cdfdd28
Erin O'Connor <erinoconnor@dhcp-10-110-244-64.v805.med.uthscsa.edu>
2025-02-11 11:55:29 -0600

    Transforming data set, adding random values to columns

---
+++ Simulate.R
@@ -0,0 +1,44 @@
+#'---
+#' title: "TSCI 5050: Simulating a Data Set"
+#' author: 'Erin Stewart, Author Two ^1^'
+#' abstract: |
+#'  | Provide a summary of objectives, study design, setting, participants,
+#'  | sample size, predictors, outcome, statistical analysis, results,
+#'  | and conclusions.
+#' documentclass: article
+#' description: 'Manuscript'
+#' clean: false
+#' self_contained: true
+#' number_sections: false
+#' keep_md: true
+#' fig_caption: true
+#' output:
+#'  html_document:
+#'    toc: true
+#'    toc_float: true
+#'    code_folding: show
+#' ---
+#'
+#+ init, echo=FALSE, message=FALSE, warning=FALSE
+# init ----
+# This part does not show up in your rendered report, only in the script,
+# because we are using regular comments instead of #' comments
+debug <- 0;
+knitr::opts_chunk$set(echo=debug>-1, warning=debug>0, message=debug>0, class.output="scroll-20", attr.output='style="max-height: 150px; overflow-y: auto;"');
+
+library(ggplot2); # visualisation
+library(GGally);
+library(rio);# simple command for importing and exporting
+library(pander); # format tables
+#library(printr); # set limit on number of lines printed
+library(broom); # allows to give clean dataset
+library(dplyr); #add dplyr library
+
+options(max.print=500);
+panderOptions('table.split.table',Inf); panderOptions('table.split.cells',Inf);
+whatisthis <- function(xx){
+  list(class=class(xx),info=c(mode=mode(xx),storage.mode=storage.mode(xx)
+                              ,typeof=typeof(xx)))};
+# Import Data ----
+DataFile0 <-"Data/R test data.xlsx"
+Dat0 <- import(DataFile0)
